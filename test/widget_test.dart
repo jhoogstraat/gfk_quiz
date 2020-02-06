2b2dd5992bad486439d90097bf00da6e2255761a
jhoogstraat <34964599+jhoogstraat@users.noreply.github.com>
2020-02-06 21:59:04 +0100

    fix answer III 8 77

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,37 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
+import 'dart:math';
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:gfk_questionnaire/main.dart';
+import '../lib/utils/unique_random.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+  test('Random Int Generator', () {
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    Map<int, int> counter = {};
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    Random()
+        .uniqueInts(300, 20)
+        .forEach((z) => counter.update(z, (v) => v + 1, ifAbsent: () => 1));
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    Random()
+        .uniqueInts(300, 20)
+        .forEach((z) => counter.update(z, (v) => v + 1, ifAbsent: () => 1));
+
+    Random()
+        .uniqueInts(300, 20)
+        .forEach((z) => counter.update(z, (v) => v + 1, ifAbsent: () => 1));
+
+    Random()
+        .uniqueInts(300, 20)
+        .forEach((z) => counter.update(z, (v) => v + 1, ifAbsent: () => 1));
+
+    Random()
+        .uniqueInts(300, 20)
+        .forEach((z) => counter.update(z, (v) => v + 1, ifAbsent: () => 1));
+
+    Random()
+        .uniqueInts(300, 20)
+        .forEach((z) => counter.update(z, (v) => v + 1, ifAbsent: () => 1));
+
+    print(counter);
   });
 }
